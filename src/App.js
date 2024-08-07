570bd30b345275a87fb770bdf8d0ef643e237ae6
mishrasatyapriya9 <mishrasatyapriya9@gmial.com>
2024-08-07 09:32:27 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import About from "./pages/About.js";
 import Pagenotfound from "./pages/Pagenotfound.js";
 import ScrollToTop from "./pages/ScrollToTop.js";
 import "react-toastify/dist/ReactToastify.css";
-import ContactBack from "../../client/src/components/ContactBG/Backgroundformts.jsx"
 function App() {
   return (
     <>
@@ -13,7 +12,6 @@ function App() {
         <Route path="/" element={<Homepage />} />
         <Route path="/aboutus" element={<About />} />
         <Route path="*" element={<Pagenotfound />} />
-        <Route path="/ContactBack" element={<ContactBack />} />
       </Routes>
     </>
   );
